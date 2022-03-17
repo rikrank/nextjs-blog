0c994efe285a22218846bedcf781ead35bac866a
rikrank <morishita-r@tam-tam.co.jp>
2022-03-18 01:53:39 +0900

    add: テスト

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home({ allPostsData }) {
       <section>
         <p>[Your Self Introduction]</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
+          (This is a sample website - you’ll be building a site like this on ¥¥¥¥¥¥
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
